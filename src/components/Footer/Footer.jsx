989d38f1f800f67b8dae06808ab672a7b603d4a1
Noah Peters <nope@umich.edu>
2023-02-27 14:42:12 -0500

    deployed website

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,17 @@
-import { AiFillGithub } from 'react-icons/ai';
-import './styles.css';
+import { AiFillGithub } from "react-icons/ai";
+import "./styles.css";
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer-wrapper">
         <p>by Noah Peters, nope@umich.edu,</p>
-        <a href="/"><AiFillGithub /></a>
+        <a href="https://github.com/noahpete/theory-vis.git">
+          <AiFillGithub />
+        </a>
       </div>
     </footer>
   );
-}
- 
+};
+
 export default Footer;
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Footer } from "./Footer/Footer";
 export { default as Button } from "./Button/Button";
+export { default as About } from "./About/About";
 
 // visualization components
 export { default as VisContainer } from "./VisContainer/VisContainer";
