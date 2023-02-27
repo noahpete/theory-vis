7c945983afb66ad283eb803f6bf76ee459dfbabe
Noah Peters <nope@umich.edu>
2023-02-27 17:27:24 -0500

    basic publish

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, Algorithms, Cryptography, Computability } from "./pages";
+import { Home, About, Algorithms, Cryptography, Computability } from "./pages";
 import { Navbar, Footer } from "./components";
 import "./index.css";
 
 function App() {
   return (
-    <Router basename={`${process.env.PUBLIC_URL}`}>
+    <Router>
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        {/* <Route exact path="/theory-vis/about" element={<About />} /> */}
+        <Route exact path="/about" element={<About />} />
         <Route exact path="/algorithms" element={<Algorithms />} />
         <Route exact path="/computability" element={<Computability />} />
         <Route exact path="/cryptography" element={<Cryptography />} />
