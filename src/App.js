65f2294cc2bfa2356488eab37373ef5f8393b323
Noah Peters <nope@umich.edu>
2023-02-27 16:41:00 -0500

    hashrouter change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, Algorithms, Cryptography, Computability } from "./pages";
 import { Navbar, Footer } from "./components";
 import "./index.css";
