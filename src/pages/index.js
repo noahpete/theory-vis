71ac1d6b9001a90e462ba47f7842e657a1564f1d
Noah Peters <nope@umich.edu>
2023-03-07 21:31:55 -0500

    Updates

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home/Home";
-export { default as About } from "./About/About";
-export { default as Algorithms } from "./Algorithms/Algorithms";
-export { default as Cryptography } from "./Cryptography/Cryptography";
-export { default as Computability } from "./Computability/Computability";
