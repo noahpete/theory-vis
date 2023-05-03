cb7a9f0b49690e01eabaedbaa884f5f031883034
Noah Peters <nope@umich.edu>
2023-05-03 15:51:36 -0400

    rudimentary closest pair working, but wrong

--- src/components/ClosestPair/ClosestPair.jsx
+++ src/components/ClosestPair/ClosestPair.jsx
@@ -3,27 +3,16 @@ import { Pseudocode } from "components";
 import Dot from "./Dot";
 import "./styles.css";
 
-const util = require("utils/NumberUtils");
 const CANVAS_DIMEN = 300;
 const ANIMATION_DELAY = 100;
-
-const COLORS = {
-  red: "#EF233C",
-  yellow: "#FFBE0B",
-  blue: "#3A86FF",
-};
 
 class ClosestPair extends Component {
   state = {
     dots: [],
-    dotsSteps: [],
-    colorKey: [],
-    colorSteps: [],
     curStep: 0,
     count: 10,
-    delay: 100,
-    algorithm: "",
-    timeouts: [],
+    stopAnimation: false,
+    lineCoords: { x1: 0, y1: 0, x2: 0, y2: 0 },
   };
 
   componentDidMount() {
@@ -31,67 +20,171 @@ class ClosestPair extends Component {
   }
 
   generateRandomDots = (n = 10) => {
-    this.setState({ dots: [] });
+    this.setState({ dots: [], seed: Math.random() });
+    this.drawLine({ x: 0, y: 0 }, { x: 0, y: 0 });
     let newDots = [];
-
     for (let i = 0; i < n; ++i) {
       newDots[i] = {
         id: i,
-        y: Math.random() * 0.75 * CANVAS_DIMEN,
-        x: Math.random() * 0.75 * CANVAS_DIMEN,
+        y: Math.random() * 0.95 * CANVAS_DIMEN + 2,
+        x: Math.random() * 0.95 * CANVAS_DIMEN + 2,
         color: "darkgray",
+        visited: false,
       };
     }
-
     return newDots;
   };
 
-  animateDots = (newDots) => {
-    this.setState({ dots: [] });
+  animateDots = async (newDots) => {
+    this.setState({ dots: [], stopAnimation: true });
     let delay = 0;
-    newDots.forEach((dot) => {
-      setTimeout(() => {
-        this.setState((prevState) => ({
-          dots: [...prevState.dots, dot],
-        }));
-      }, delay);
-      delay += ANIMATION_DELAY;
+    for (const dot of newDots) {
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      this.setState((prevState) => ({
+        dots: [...prevState.dots, dot],
+      }));
+      // delay += ANIMATION_DELAY;
+    }
+    this.setState({ stopAnimation: false });
+  };
+
+  visitDot = (dotIdx) => {
+    this.setState((prevState) => {
+      const updatedDots = [...prevState.dots];
+      updatedDots[dotIdx] = {
+        x: prevState.dots[dotIdx].x,
+        y: prevState.dots[dotIdx].y,
+        visited: true,
+      };
+      return { dots: updatedDots };
     });
   };
 
-  changeDots = (index, dot) => {
-    let temp = this.state.dots;
-    temp[index] = dot;
-    this.setState({
-      dots: temp,
-      dotsSteps: [temp],
-      curStep: 0,
+  bestDot = (dotIdx) => {
+    this.setState((prevState) => {
+      const updatedDots = [...prevState.dots];
+      updatedDots[dotIdx] = {
+        x: prevState.dots[dotIdx].x,
+        y: prevState.dots[dotIdx].y,
+        isBest: true,
+      };
+      return { dots: updatedDots };
     });
   };
 
-  naive = (Points) => {
-    let minDist = Infinity;
-    let minPair = [];
+  doiDot = (dotIdx) => {
+    this.setState((prevState) => {
+      const updatedDots = [...prevState.dots];
+      updatedDots[dotIdx] = {
+        x: prevState.dots[dotIdx].x,
+        y: prevState.dots[dotIdx].y,
+        doi: !prevState.dots[dotIdx].doi,
+      };
+      return { dots: updatedDots };
+    });
+  };
 
-    for (let i = 0; i < Points.length; ++i) {
-      for (let j = i + 1; j < Points.length; ++j) {
-        const dist = Math.sqrt(
-          (Points[i].x - Points[j].x) * (Points[i].x - Points[j].x) +
-            (Points[i].y - Points[j].y) * (Points[i].y - Points[j].y)
-        );
-        if (dist < minDist) {
-          minDist = dist;
-          minPair = [i, j];
+  drawLine = (dot1, dot2) => {
+    const { x: x1, y: y1 } = dot1;
+    const { x: x2, y: y2 } = dot2;
+    this.setState({ lineCoords: { x1, y1, x2, y2 } });
+  };
+
+  animateAlgorithm = (log) => {
+    console.log(this.state.stopAnimation);
+    let i = 0;
+    const origSeed = this.state.seed;
+    const animate = () => {
+      if (i < log.length) {
+        if (this.state.stopAnimation || this.state.seed != origSeed) {
+          return;
+        }
+        const elt = log[i];
+        switch (elt.action) {
+          case "doi":
+            this.doiDot(elt.dotIdx);
+            break;
+          case "best":
+            this.bestDot(elt.dotIdx);
+            break;
+          case "visit":
+            this.visitDot(elt.dotIdx);
+            break;
+          case "line":
+            this.drawLine(
+              this.state.dots[elt.dot1Idx],
+              this.state.dots[elt.dot2Idx]
+            );
+            break;
+          default:
+            break;
+        }
+        i++;
+        setTimeout(animate, 500);
+      }
+    };
+    animate();
+  };
+
+  // algorithms
+  naive = () => {
+    console.log("naive");
+    const log = [];
+    let dMin = Infinity;
+    for (let i = 0; i < this.state.dots.length; i++) {
+      const curState = {
+        v: this.state.dots[i].visited,
+        b: this.state.dots[i].isBest,
+        i: this.state.dots[i].doi,
+      };
+      log.push({
+        dotIdx: i,
+        action: "doi",
+      });
+      let isMin = false;
+      for (let j = i + 1; j < this.state.dots.length; j++) {
+        log.push({
+          dot1Idx: i,
+          dot2Idx: j,
+          action: "line",
+        });
+        const dx = Math.pow(this.state.dots[i].x - this.state.dots[j].x, 2);
+        const dy = Math.pow(this.state.dots[i].y - this.state.dots[j].y, 2);
+        const dist = Math.sqrt(dx + dy);
+        if (dist < dMin) {
+          isMin = true;
+          log.push({
+            dotIdx: i,
+            action: "best",
+          });
+          log.push({
+            dotIdx: j,
+            action: "best",
+          });
+          dMin = dist;
         }
       }
+      log.push({
+        dotIdx: i,
+        action: curState.b || isMin ? "best" : "visit",
+      });
     }
-    return minPair;
+    this.animateAlgorithm(log);
   };
 
   render() {
     let dots = this.state.dots.map((dot, index) => (
-      <Dot key={index} x={dot.x} y={dot.y} color={dot.color} />
+      <Dot
+        key={index}
+        x={dot.x}
+        y={dot.y}
+        visited={dot.visited}
+        isBest={dot.isBest}
+        doi={dot.doi}
+      />
     ));
+
+    console.log(this.state.lineCoords);
 
     return (
       <div className="closest-pair">
@@ -99,28 +192,29 @@ class ClosestPair extends Component {
           <div className="cp-pseudocode">
             <Pseudocode />
           </div>
-          <div className="cp-canvas">{dots}</div>
+          <div className="cp-canvas">
+            {dots}
+            <svg width={CANVAS_DIMEN} height={CANVAS_DIMEN}>
+              <line
+                x1={this.state.lineCoords.x1}
+                y1={this.state.lineCoords.y1}
+                x2={this.state.lineCoords.x2}
+                y2={this.state.lineCoords.y2}
+                stroke="red"
+                strokeWidth="2"
+              />
+            </svg>
+          </div>
         </div>
         <div className="cp-controls">
           <div className="cp-buttons-wrapper">
             <button onClick={() => this.animateDots(this.generateRandomDots())}>
               Make Dots
             </button>
-            <button
-              onClick={() => {
-                console.log(dots);
-                let newDots = [...dots];
-                let pair = this.naive(dots);
-                newDots[0].color = "blue";
-                newDots[1].color = "blue";
-                console.log(pair);
-                this.setState({
-                  dots: newDots,
-                });
-              }}
-            >
-              NaiveClosestPair
-            </button>
+            <button onClick={() => this.naive()}>NaiveClosestPair</button>
+            <button onClick={() => this.visitDot(0)}>Make dot 0 blue</button>
+            <button onClick={() => this.bestDot(0)}>Make dot 0 best</button>
+            <button onClick={() => this.doiDot(0)}>Make dot 0 doi</button>
           </div>
         </div>
       </div>
--- src/components/ClosestPair/Dot.jsx
+++ src/components/ClosestPair/Dot.jsx
@@ -2,36 +2,18 @@ import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import "./styles.css";
 
-const Dot = ({ x, y, color, animation }) => {
+const Dot = ({ x, y, visited, isBest, doi, animation }) => {
   return (
-    <motion.div
-      className="dot"
+    <div
+      className={`dot ${
+        doi ? "dot-doi" : isBest ? "dot-best" : visited ? "dot-visited" : ""
+      }`}
       style={{
         left: x,
         top: y,
-        backgroundColor: color,
       }}
-      initial={{ scale: 0 }}
-      animate={{ rotate: 180, scale: 1 }}
-      transition={{
-        type: "spring",
-        stiffness: 260,
-        damping: 20,
-      }}
-    ></motion.div>
+    ></div>
   );
 };
 
 export default Dot;
-
-// from ClosestPair.jsx
-// useEffect(() => {
-//   generateRandomDots(20);
-// }, []);
-
-//
-// CHANGE TO "for each pair of points, p1, p2 E P:"
-//
-//
-// CHANGE TO "for each pair of points, p1, p2 E P:"
-// in the latex part i mean
